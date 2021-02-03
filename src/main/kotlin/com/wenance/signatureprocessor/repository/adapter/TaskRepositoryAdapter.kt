52a581a22c146f59be54e21cf983fdfafc320338
Jesus <jh.ledesmaa@gmail.com>
2021-02-03 18:34:11 -0300

    findAndAddSteps

--- src/main/kotlin/com/wenance/signatureprocessor/repository/adapter/TaskRepositoryAdapter.kt
+++ src/main/kotlin/com/wenance/signatureprocessor/repository/adapter/TaskRepositoryAdapter.kt
@@ -6,8 +6,6 @@ import com.wenance.signatureprocessor.core.model.Task
 import com.wenance.signatureprocessor.repository.TaskRepository
 import com.wenance.signatureprocessor.repository.dao.StepDao
 import com.wenance.signatureprocessor.repository.dao.TaskDao
-import com.wenance.signatureprocessor.repository.domain.TaskEntity
-import com.wenance.signatureprocessor.utils.ModelMapper
 import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 import kotlinx.coroutines.reactive.awaitSingleOrNull
@@ -21,13 +19,14 @@ class TaskRepositoryAdapter(val taskDao: TaskDao, val stepDao: StepDao) : TaskRe
     }
 
     override suspend fun findByHashId(task: Task): Task? =
-            taskDao.findByHashId(task.hashId).map { t -> t.toTask() }.awaitFirstOrNull()?.apply { findSteps(this) }
+            taskDao.findByHashId(task.hashId).map { t -> t.toTask() }.awaitFirstOrNull()?.apply { findAndAddSteps(this) }
 
 
 
 
 
-    private suspend fun findSteps(task: Task): Set<Step> {
+
+    private suspend fun findAndAddSteps(task: Task): Set<Step> {
         return task.steps
             .map {
                 stepDao.findById(it.id!!).map { step -> step.toStep() }.awaitSingleOrNull()
